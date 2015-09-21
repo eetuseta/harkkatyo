5136515227fc3d8c4de41caae1d7ced1c9a088ba
dalud <jaakko.niska@gmail.com>
2015-09-21 13:00:32 +0300

    kopion nuo maksuupääte, tili, blaa blaa luokat vanhoista. Katotaan tullaanko niitä kaikkia käyttäänkään...

--- Harkkatyo/src/harkkatyo/Pankkitili.java
+++ Harkkatyo/src/harkkatyo/Pankkitili.java
@@ -1,5 +1,3 @@
-package harkkatyo;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,8 +6,39 @@ package harkkatyo;
 
 /**
  *
- * @author Tony
+ * @author jaakk_000
  */
 public class Pankkitili {
+    private double rahamaara;
     
+    //konstruktorit
+    public Pankkitili(){
+            }
+    public Pankkitili(double rahamaara){
+        this.rahamaara=rahamaara;
+    }
+    
+    //metodit
+    public void setRaha(double rahamaara){
+        this.rahamaara=rahamaara;
+    }
+    public double getRahamaara(){
+        return rahamaara;
+    }
+    public void lisaa(double pano){
+        rahamaara=rahamaara+pano;
+    }
+    boolean vahenna(double otto){
+        if (otto>rahamaara){
+            System.out.println("tilillä ei ole katetta.");
+            return false;            
+        }
+        else {
+            rahamaara=rahamaara-otto;
+            return true;
+                    } 
 }
+}
+
+    
+

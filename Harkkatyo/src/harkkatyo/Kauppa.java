a18e7e1d6def2512ea6234d0b01381945c1f8298
Tony <Tony@Tonykone.wlan.metropolia.fi>
2015-09-22 13:50:57 +0300

    Kauppa ja Ui kommentoitu

--- Harkkatyo/src/harkkatyo/Kauppa.java
+++ Harkkatyo/src/harkkatyo/Kauppa.java
@@ -1,28 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+// Main ohjelma, sekä kauppa class jonka kautta kaikki tieto kulkee UI:lta Olioille
 package harkkatyo;
 
-/**
- *
- * @author Eetu
- */
-public class Kauppa {
 
+public class Kauppa {
+        
+        //Kauppaolion ominaisuudet
+        
         Myyja myyja = new Myyja();
         Asiakas asiakas = new Asiakas();
         Levyt levyt = new Levyt();
         UI ui = new UI();
         
-        public void kaynnista(){
-        ui.Aloitalevykauppa();
-        }
         
         //Metodit
-        public boolean getOnkolevya(String haluttulevy){
-            if (asiakas.onkoLevya(haluttulevy) == true){
+        
+        public void kaynnista(){                            //Tämä on ensimmäinen ohjelman osa, käynnistää UI:n
+            ui.Aloitalevykauppa();
+        }
+                
+        public boolean getOnkolevya(String haluttulevy){    
+            if (asiakas.onkoLevya(haluttulevy)==true){
                 return true;
             }
                 else {
@@ -30,9 +28,11 @@ public class Kauppa {
             }      
         }
         
+        //Varsinainen main -
+        
     public static void main(String[] args) {
-        Kauppa kauppa = new Kauppa();
-        kauppa.kaynnista();
+        Kauppa kauppa = new Kauppa();       //luodaan kauppaolio
+        kauppa.kaynnista();                 //käynnistetään metodi joka käynnistää UI:n 
 
     
     }
--- Harkkatyo/src/harkkatyo/UI.java
+++ Harkkatyo/src/harkkatyo/UI.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Viewer class jossa tapahtuu kaikki kommunikointi käyttäjän kanssa
+
 package harkkatyo;
+
 import java.util.Scanner;
-/**
- *
- * @author jaakk_000
- */
+
 public class UI {
     static Scanner lukija = new Scanner(System.in);
     
@@ -25,5 +20,5 @@ public class UI {
         haluttulevy=lukija.next();
         kauppa.getOnkolevya(haluttulevy);
     }
-
+    
 }

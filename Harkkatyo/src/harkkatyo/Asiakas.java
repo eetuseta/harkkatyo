36972a6a61b1d2a3fa88379e08cae6fa997d76b7
Eetu Kaivola <eetu.kaivola@gmail.com>
2015-09-22 12:13:43 +0300

    päivitetty asiakasta

--- Harkkatyo/src/harkkatyo/Asiakas.java
+++ Harkkatyo/src/harkkatyo/Asiakas.java
@@ -13,31 +13,47 @@ import java.util.Scanner;
  */
 public class Asiakas {
 
+    private Myyja myyja;
+
+    public String levy;
     static Scanner lukija = new Scanner(System.in);
 
     public Asiakas() {
 
     }
-    //Myyja myyja = new Myyja();
-    public String levy;
 
-    public boolean onkoLevya(String levy) {
-        if (myyja.getOnkolevya(levy) == true ){
-            System.out.println("kiitos, ostanpa sen pois");
-            //kiitos, ostanpa sen pois
+    void Myyja(Myyja myyja) {
+        this.myyja = myyja;
+    }
+    
+    public boolean onkoLevya (String levy){
+        if (!myyja.getOnkolevya){
+            System.out.println("voi räkä."); //testisyöte
+            return false;
+        }
+        else {
+            System.out.println("ostanpa pois kuleksimasta.");//testisyöte
             return true;
         }
-        else (myyja.getOnkolevya(levy) == false){
-        
-        System.out.println("voi räkä.");
-        return false;
-        // voi rähmä
     }
+
+    /*boolean onkoLevya(String levy) {
+     if (myyja.getOnkolevya(levy) == true) {
+     System.out.println("kiitos, ostanpa sen pois");
+     //kiitos, ostanpa sen pois
+     return true;
+     } else {
+     (myyja.getOnkolevya(levy) == false)
+     {
+
+     System.out.println("voi räkä.");
+     return false;
+     // voi rähmä
+     }*/
         //tehdään kysely myyjälle. löytyykö levyä x
-        //System.out.println("Katsot ympärillesi hämmentyneenä ja käännyt myyjän puoleen.");
-        //System.out.println("Hei, onko teillä: " + lukija.next + "levyä?" );
-
-    }
-
-    //selaa levyjä kirjaimen "d" kohdalta
+    //System.out.println("Katsot ympärillesi hämmentyneenä ja käännyt myyjän puoleen.");
+    //System.out.println("Hei, onko teillä: " + lukija.next + "levyä?" );
 }
+
+//selaa levyjä kirjaimen "d" kohdalta
+

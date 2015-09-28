6ef6e22c1661d6796370f6a7cb07076aee2ebab7
Eetu Kaivola <eetu.kaivola@gmail.com>
2015-09-28 12:07:20 +0300

    levyjen selaus tehty

--- Harkkatyo/src/harkkatyo/Asiakas.java
+++ Harkkatyo/src/harkkatyo/Asiakas.java
@@ -1,38 +1,48 @@
 package harkkatyo;
 
 import java.util.*;
-
+import javax.swing.JOptionPane;
 
 public class Asiakas {
-    
-    //muuttujat
 
+    //muuttujat
     private Myyja myyja;
     private Lompakko lompakko;
     public int hinta;
+    private Levyt levyt;
 
     public String levy;//teksti
-    
+
     //luodaan skanneri
     static Scanner lukija = new Scanner(System.in);
-    
+
     //asiakkaan konstruktori
     public Asiakas() {
 
     }
-    public void Lompakko(Lompakko lompakko){
+
+    //levyjen konstruktori
+
+    public void Levyt(Levyt levyt) {
+        this.levyt = levyt;
+    }
+
+    //lompakon konstruktori
+    public void Lompakko(Lompakko lompakko) {
         this.lompakko = lompakko;
     }
+
     //myyjän konstruktori
     public void Myyja(Myyja myyja) {
         this.myyja = myyja;
     }
+
     //levyn kysely
     public List<Levy> onkoLevya(String levy) {
         return myyja.getOnkolevya(levy);
 
     }
-    
+
     //levy halutaan ostaa, ja ostetaan jos on rahaa.
     public boolean maksaLevy(int rahamaara) {
         if (lompakko.getRahamaara() > hinta) {
@@ -43,4 +53,34 @@ public class Asiakas {
         }
     }
 
+    public String selaaLevja(String selaa) {
+        String palautus = "";
+        String merkki = JOptionPane.showInputDialog("Miltä kohdalta selataan? (A-Z");
+        char cmerkki = merkki.charAt(0);
+        for (Levy i : levyt.levyt) {
+            if (cmerkki == ((i.artisti).charAt(0))) {
+                palautus = palautus + i + "/n";
+                
+            }
+            
+
+        }
+        return palautus;
+    }
 }
+    
+
+    /*ArrayList<Levy> hakutulos = new ArrayList();
+     String loytyi = null;
+
+     for (Levy levy : levyt) {
+     if (haku.equalsIgnoreCase(levy.artisti) || haku.equalsIgnoreCase(levy.levynNimi)) {
+     hakutulos.add(levy);
+     }
+     }
+     if (hakutulos.size() == 0) {
+     return null;
+     }
+     return hakutulos;
+     }*/
+
